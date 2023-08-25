46e24e595e2ccab67aee5f8e22bb61d6a883c313
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-08-25 12:49:35 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,11 +4,12 @@ const ctrl = require("../../controllers/auth");
 
 const { validateBody, authenticate } = require("../../middlewares");
 
-const { registerLoginSchema } = require("../../models/user");
+const {
+	registerLoginSchema,
+	updateSubscriptionSchema,
+} = require("../../models/user");
 
 const router = express.Router();
-
-const { schemas } = require("../../models/contact");
 
 router.post("/register", validateBody(registerLoginSchema), ctrl.register);
 
@@ -21,7 +22,7 @@ router.post("/logout", authenticate, ctrl.logout);
 router.patch(
 	"/:id/subscription",
 	authenticate,
-	validateBody(schemas.updateSubscriptionSchema),
+	validateBody(updateSubscriptionSchema),
 	ctrl.updateSubscription
 );
 
